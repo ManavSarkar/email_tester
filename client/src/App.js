dd3e525b272e8d1e92bcdcecd727d26a7eb6118d
Manav Sarkar <42129636+ManavSarkar@users.noreply.github.com>
2023-02-23 17:01:46 +0530

    init

---
+++ client/src/App.js
@@ -0,0 +1,61 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+import Navbar from './components/Navbar';
+
+function App() {
+  const [emails, setEmails] = useState({})
+  const [emailHTML, setEmailHTML] = useState('')
+  const getEmails = async () => {
+    const response = await fetch('/apirefreshmails');
+    const data = await response.json();
+
+    var emails = data['emails'];
+    setEmails(emails);
+    console.log("Emails refreshed");
+  }
+
+  const displayEmail = (id) => {
+    // console.log(emails[id]['html']);
+    setEmailHTML(emails[id]['html']);
+  }
+  useEffect(() => {
+    getEmails();
+  }, []);
+
+  return (
+    <div className="App">
+      <Navbar getEmails={getEmails} />
+
+      <div className='flex flex-auto grid-flow-col'>
+        <div className='row-span-1 bg-slate-400 w-1/4 h-full fixed'>
+          <p className='text text-xl p-4 font-bold'>Email IDs</p>
+          <ul className='list-none list-inside'>
+            {Object.keys(emails).map((key) => {
+              return <li key={key}>
+                <div onClick={() => displayEmail(key)}>
+                  <p className='btn btn-ghost' >
+                    {emails[key]['envelope_to']}
+
+                  </p>
+                  {/* date */}
+                  <p className='text text-sm'>{new Date(emails[key]['date']).toString()}</p>
+
+                </div>
+              </li>
+            })}
+          </ul>
+
+        </div>
+        <div className='row-span-2 bg-red-300 w-3/4 h-full fixed left-1/4'>
+          <p className='text text-xl p-4 font-bold'>Emails</p>
+
+          <div dangerouslySetInnerHTML={{ __html: emailHTML }} />
+
+        </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const axios = require('axios');
+const dotenv = require('dotenv');
+const jwt = require('jsonwebtoken');
+const cors = require('cors');
+
+app.use(cors());
+dotenv.config();
+
+
+
+const port = process.env.PORT || 5000;
+
+// use frontend build folder if production
+if (process.env.NODE_ENV === 'production') {
+}
+
+app.use(express.static('client/build'));
+
+
+
+// get all mails
+app.get('/apirefreshmails', (req, res) => {
+    const APIKEY = process.env.API_KEY;
+    const NAMESPACE = process.env.NAMESPACE;
+    axios.get(`https://api.testmail.app/api/json?apikey=${APIKEY}&namespace=${NAMESPACE}&pretty=true&livequery=true`)
+        .then(response => {
+            // console.log(response.data);
+            res.json(response.data);
+        })
+        .catch(error => {
+            console.log(error);
+            res.send(error);
+        });
+});
+
+
+
+app.listen(port, () => {
+    console.log('Listening on port 3000');
+}
+);
